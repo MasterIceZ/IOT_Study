9bc1c13ee082c13575cd0b8b07c66bec7e20e8bf
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2021-06-28 16:14:57 +0700

    Study

---
+++ 28-6-PWM/28-6-PWM.ino
@@ -0,0 +1,24 @@
+#define LED_PIN D0
+int val = 0;
+
+// analogWrite -> Write as u_int [Not Boolean]
+
+// Digital -> Boolean [True(1)/ False(0)]
+// Analog -> PWM (Pluse with Modulation)
+
+void setup() 
+{
+    pinMode(LED_PIN,OUTPUT);
+}
+void loop() 
+{
+    for(val=0; val<(1 << 10); val += 10){
+      analogWrite(LED_PIN, val);
+      delay(30);
+    }
+    for(; val>=0; val -= 10){
+      analogWrite(LED_PIN, val);
+      delay(30);
+    }
+    delay(1000);
+}
